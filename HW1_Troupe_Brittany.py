8cd388e892ffc9a7d7f5130eb6b64266040a6b21
BrittTr <97695069+BrittTr@users.noreply.github.com>
2022-01-26 21:47:38 -0500

    Add files via upload

---
+++ HW1_Troupe_Brittany.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+nums = list(range(30, 61, 5))
+print(nums)
+
+print(nums[:: -1])
+
+nums.append(65)
+nums = nums[:: -1]
+print(nums)
+
+
+# In[2]:
+
+
+newlist = []
+for i in range(0,21):
+    newlist.append(i)
+print(newlist)
+
+newlist.remove(0)
+print(newlist)
+
+print(len(newlist))
+print(max(newlist))
+print(min(newlist))
+
+total = 0
+for i in newlist:
+    total += i
+print(total)
+
+
+# In[3]:
+
+
+weather = {'sunny': 'play', 'rainy': 'watch TV', 'cloudy': 'walk'}
+
+for key, value in weather.items():
+    print('when', key, 'let us', value)   
+    
+weather['snowy'] = 'ski' 
+
+
+# In[ ]:
+
+
+
+
